1852dd4dfbc4ff82af850d301bc7520de562f5e5
J-Falconetti <jospeh.falcoentti@gmail.com>
2019-02-27 15:52:10 -0500

    working losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,13 @@
 // this is the Javascrpit 
-
+// setting letter
 var letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 var compPick = letter[Math.floor(Math.random() * letter.length)];
+// letter set getting user guess
 var userGuess;
 var win=0;
 var loss=0;
+var losstext = document.getElementById("losses");
 
 console.log(compPick)
 document.onkeypress = function(event){
@@ -15,9 +17,14 @@ document.onkeypress = function(event){
 if(userGuess === compPick) {
     win++;
     alert("you win! " +win);
+    document.write("wins " + win);
+    
 
 } else {
     loss ++
-    alert("you lose! " +loss)
+    losstext.textContent=" Losses: " +loss;
+    alert("you lose! " +loss);
+  
+    
 }
 }
