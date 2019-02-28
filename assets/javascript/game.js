c500cec909e31cfbe6dc764c31dc845961071e27
J-Falconetti <jospeh.falcoentti@gmail.com>
2019-02-27 20:16:30 -0500

    win and loss counter working.  need to add guess counter and reset wildcard letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,38 @@
 // this is the Javascrpit 
 // setting letter
-var letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var compPick = letter[Math.floor(Math.random() * letter.length)];
 // letter set getting user guess
 var userGuess;
-var win=0;
-var loss=0;
+var wins = 0;
+var loss = 0;
 var losstext = document.getElementById("losses");
+var winstext = document.getElementById("wins");
 
 console.log(compPick)
-document.onkeypress = function(event){
+document.onkeypress = function (event) {
     userGuess = event.key;
     console.log(userGuess)
 
-if(userGuess === compPick) {
-    win++;
-    alert("you win! " +win);
-    document.write("wins " + win);
-    
 
-} else {
-    loss ++
-    losstext.textContent=" Losses: " +loss;
-    alert("you lose! " +loss);
-  
-    
+
+
+    if (userGuess === compPick) {
+        wins++;
+        winstext.textContent = " Wins: " + wins;
+        alert("you win! " + wins);
+
+
+    }
+    else {
+        loss++
+        losstext.textContent = " Losses: " + loss;
+        alert("you lose! " + loss);
+    }
+
+
+
+
 }
-}
+
