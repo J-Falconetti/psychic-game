4a392e9d55806506cab24616f74c99911ea26e11
J-Falconetti <jospeh.falcoentti@gmail.com>
2019-02-28 12:23:35 -0500

    working(ish)  project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,58 @@
 // this is the Javascrpit 
 // setting letter
+// 
 var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-var compPick = letter[Math.floor(Math.random() * letter.length)];
-// letter set getting user guess
+var compPick;
+var guess = 1;
 var userGuess;
 var wins = 0;
 var loss = 0;
 var losstext = document.getElementById("losses");
 var winstext = document.getElementById("wins");
+var guesstext = document.getElementById("guesstx");
 
-console.log(compPick)
+getlet()
+// ADD GET LETTER FUNCTION to recall for more letter when needed
+function getlet() {
+    compPick = letter[Math.floor(Math.random() * letter.length)];
+    // letter set getting user guess
+    console.log(compPick)
+}
 document.onkeypress = function (event) {
+    
     userGuess = event.key;
     console.log(userGuess)
-
-
 
 
     if (userGuess === compPick) {
         wins++;
         winstext.textContent = " Wins: " + wins;
         alert("you win! " + wins);
-
-
+        getlet();
+        
     }
     else {
-        loss++
+        
         losstext.textContent = " Losses: " + loss;
-        alert("you lose! " + loss);
+        console.log(guess)
+         if (guess === 10) {
+             alert("you lose! " + loss);
+             guess = 1
+             loss++
+             getlet();
+         }
+
+
+         else {
+            guesstext.textContent = " Guess #: " + guess;
+            guess++;
+            alert("Guess Again");
+        }
     }
-
-
-
-
 }
+
+
+
+
+
 
